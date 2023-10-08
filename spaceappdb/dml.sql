94c55c7b3b0368b7b165c3eb54931b2a61046c59
richardfm77 <ricardo_fm77@ciencias.unam.mx>
2023-10-08 04:33:58 -0600

    query to insert

--- spaceappdb/dml.sql
+++ spaceappdb/dml.sql
@@ -1,1 +1,8 @@
+-- Users
 INSERT INTO userApp(nameUserApp,mail,userPassword) VALUES	('fulanito', 'fulanito@gmail.com', '123');
+
+-- WaterBody
+
+INSERT INTO "waterbody" ("namewaterbody", "typeof", "plants", "animals", "waterquality", "long", "lat", "organization") VALUES ('Parque Tezozómoc', 'Lago', 'Papiro ', 'Garzas Blancas, Palas Cucharas del Norte, Peces Carpa, Tortugas Japonesas', 'No apta para nado', -99.21025, 19.499546, 'Gobierno de la Ciudad de México');
+INSERT INTO "waterbody" ("namewaterbody", "typeof", "plants", "animals", "waterquality", "long", "lat", "organization") VALUES ('San Juan de Aragón', 'Lago', 'Casuarina Equisetifolia  ', 'Tilapia, Guppy silvestre', 'No apto para nado', -99.073994, 19.461344, 'Gobierno de la Ciudad de México');
+INSERT INTO "waterbody" ("namewaterbody", "typeof", "plants", "animals", "waterquality", "long", "lat", "organization") VALUES ('Nabor Carrillo', 'Lago', 'Junco', 'Mexclapique, Rana de Árbol Plegada, Correlimos de Baird, Falaropo Tricolor, Cigüeñuela de Cuello Negro, Chorlitejo Colirrojo, Bisbita Llanero', 'No apto para nado', -98.979576, 19.4675, 'Gobierno de la Ciudad de México');
